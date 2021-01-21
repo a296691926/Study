662c417e50eba59252df8935ea7027662e2544b8
a296691926 <296691926@qq.com>
2021-01-21 13:56:00 +0800

    第七次测试

---
+++ GitHub_Test/fixture_test01.py
@@ -0,0 +1,18 @@
+import pytest
+
+@pytest.fixture()
+def init_xx():
+    print("......初始化测试数据工作")
+    with open("D:\Study\APP\GitHub_Test\data.txt", "w") as f:
+        f.write('1')
+
+
+class Test_xx:
+    # 参数引用
+    def test_xx(self, init_xx):
+        with open("D:\Study\APP\GitHub_Test\data.txt", 'r') as f:
+            assert f.read() == '1'
+
+
+if __name__ == '__main__':
+    pytest.main(["-s", "fixture_test01.py"])
