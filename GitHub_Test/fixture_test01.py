24a6dc198c90c9da6abf4e1f346d4cf78009c642
a296691926 <296691926@qq.com>
2021-01-21 15:21:54 +0800

    测试自动集成

--- GitHub_Test/fixture_test01.py
+++ GitHub_Test/fixture_test01.py
@@ -13,6 +13,12 @@ class Test_xx:
         with open("D:\Study\APP\GitHub_Test\data.txt", 'r') as f:
             assert f.read() == '1'
 
+class Test_xx:
+    # 参数引用
+    def test_xx(self, init_xx):
+        with open("D:\Study\APP\GitHub_Test\data.txt", 'r') as f:
+            assert f.read() == '1'
+
 
 if __name__ == '__main__':
     pytest.main(["-s", "fixture_test01.py"])
